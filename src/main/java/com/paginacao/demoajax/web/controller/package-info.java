7045ca0daa49e84ee8818e621ce338ee8c880c13
Carlos Santos39 <novasintoniasp@gmail.com>
2022-11-13 15:12:38 -0300

    projeto ajax iniciado

---
+++ src/main/java/com/paginacao/demoajax/web/controller/package-info.java
@@ -0,0 +1,1 @@
+package com.paginacao.demoajax.web.controller;
---
+++ src/test/java/com/paginacao/demoajax/DemoAjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paginacao.demoajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAjaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
